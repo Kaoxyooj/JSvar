f06c1c21c12dc91bfa4e8d08c4df802697fc0703
Kaoxyooj <locksage@hotmail.com>
2016-01-04 13:56:03 -0600

    3rdcommit

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -15,26 +15,26 @@ var hours;
 var paid;
 var overtime;
 var total;
+var regPay;
+var otHours;
+var otPay;
+var otTotal;
+var rounded;
 
 hours = prompt("How many hours did you work?");
 paid = prompt("How much do you get paid per hour?");
 overtime= paid * 1.5;
 total = hours * paid;
+regPay= 40 * paid;
+otHours= hours - 40;
+otPay= overtime * otHours;
+otTotal	= otPay + regPay;
+rounded = Math.ceil(otTotal * 100)/100;
 
 if (hours < 41 ) {
-console.log(total);
+    console.log(total);
 }else if (hours > 40) {
-	var regPay;
-	regPay= 40 * paid;
-	var otHours;
-	otHours= hours - 40;
-	var otPay;
-	otPay= overtime * otHours;
-	var otTotal;
-	otTotal	= otPay + regPay;
-	var rounded;
-	rounded = Math.ceil(otTotal * 100)/100;
-	console.log(rounded);
+    console.log(rounded);
 }
 
 
