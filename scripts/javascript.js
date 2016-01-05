a47a331e550fb3cb7a5d2eb62c7586995955ea42
Kaoxyooj <locksage@hotmail.com>
2016-01-05 15:17:26 -0600

    last commit

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,44 +1,67 @@
 "use strict";
-/*var hours;
-var paid;
-var total;
-
-
-hours= 37;
-paid= 22.50;
-total= hours*paid;
-
-
-console.log(total);*/
-function pay(hours, paid){
 
 var hours;
-var paid;
-var overtime;
-var total;
-var regPay;
-var otHours;
+var payHour;
+var regularPay;
+var payment;
 var otPay;
 var otTotal;
-var rounded;
+var pt;
+var gTotalv;
 
-hours = prompt("How many hours did you work?");
-paid = prompt("How much do you get paid per hour?");
-overtime= paid * 1.5;
-total = hours * paid;
-regPay= 40 * paid;
-otHours= hours - 40;
-otPay= overtime * otHours;
-otTotal	= otPay + regPay;
-rounded = Math.ceil(otTotal * 100)/100;
 
-	if (hours < 41 ) {
-    	console.log(total);
-	}else if (hours > 40) {
-    	console.log(rounded);
-}
+function userInputHours() {
+	var hour;
+	hour = prompt("How many hours did you work?");
+	return hour; 	
+};
+function userInputPay() {
+	var pay;
+	pay = prompt("How much do you get paid per hour?");
+	return pay;
+};
+function regPay(hr,pa){
+	var h;
+	h = hr * pa;
+	return h;
+};
+function timeHalf(payPer){
+    var half;
+    half = payPer * 1.5
+    return half;
+};
+function overTime(hours3) {
+    var ot;
+    ot = hours3 - 40;
+    return ot;
+};
+function total(pH,oP) {	
+    var t;
+    t = pH * oP;
+    return t;
+};
+function gTotal(opt,opay) {
+    var oot;
+    oot = opt + opay;
+    return oot;
+};
+function round(r) {
+	var rounded;
+    rounded = Math.ceil(r * 100)/100;
+    return rounded;
 };
 
-pay();
+hours = userInputHours();
+payHour = userInputPay();
+regularPay = regPay(hours,payHour);
+otPay = timeHalf(payHour);
+otTotal = overTime(hours);
+payment = (hours - otTotal) * payHour;
+pt = total(otPay,otTotal);
+gTotalv = gTotal(payment,pt);
 
-
+if (hours <= 40) {
+	console.log(regularPay);
+}else if (hours > 40) {
+    console.log(round(gTotalv));
+};
